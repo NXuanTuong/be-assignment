e39848f36847f526954dbebdd77f3d753573b7ec
tancd <caotan1008@gmail.com>
2022-07-25 20:26:18 +0700

    AB-11 model category

---
+++ models/category.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const CategoryModels = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    status: {
+        type: Number
+    }
+}, {timestamps : true})
+
+export default mongoose.model("Category", CategoryModels)
